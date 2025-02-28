f95b298382c5be48f103b64b5e69ce6f36286af2
Nishad <mahmudnishad253@gmail.com>
2025-03-01 01:09:58 +0600

    clear activity

---
+++ js/clearHistory.js
@@ -0,0 +1,4 @@
+document.getElementById('clear-history').addEventListener('click', function() {
+    const activityLog = document.getElementById('activity-log');
+    activityLog.innerHTML = '<div class="px-4"></div>';
+  });
